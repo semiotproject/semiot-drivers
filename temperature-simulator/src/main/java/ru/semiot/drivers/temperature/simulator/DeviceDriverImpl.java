55ca8f3186c114d2a0c4efd4c224cd647edf22b1
Maxim Kolchin <kolchinmax@gmail.com>
2016-07-13 00:17:21 +0300

    Fixed a NPE

--- temperature-simulator/src/main/java/ru/semiot/drivers/temperature/simulator/DeviceDriverImpl.java
+++ temperature-simulator/src/main/java/ru/semiot/drivers/temperature/simulator/DeviceDriverImpl.java
@@ -1,19 +1,9 @@
 package ru.semiot.drivers.temperature.simulator;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import org.eclipse.californium.core.CoapClient;
-import org.eclipse.californium.core.CoapHandler;
 import org.eclipse.californium.core.CoapObserveRelation;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.WebLink;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.osgi.service.cm.ConfigurationException;
@@ -28,8 +18,19 @@ import ru.semiot.platform.deviceproxyservice.api.drivers.DeviceProperties;
 import ru.semiot.platform.deviceproxyservice.api.drivers.DriverInformation;
 import ru.semiot.platform.deviceproxyservice.api.drivers.Observation;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
- *
  * @author Daniil Garayzuev <garayzuev@gmail.com>
  */
 public class DeviceDriverImpl implements DeviceDriver, ManagedService {
@@ -39,92 +40,124 @@ public class DeviceDriverImpl implements DeviceDriver, ManagedService {
   private final Configuration configuration = new Configuration();
   private final DriverInformation info
       = new DriverInformation(Keys.DRIVER_PID,
-          URI.create("https://raw.githubusercontent.com/semiotproject/semiot-drivers/"
-              + "master/temperature-simulator/"
-              + "src/main/resources/ru/semiot/drivers/temperature/simulator/prototype.ttl#Mercury270"));
+      URI.create("https://raw.githubusercontent.com/semiotproject/semiot-drivers/"
+          + "master/temperature-simulator/"
+          + "src/main/resources/ru/semiot/drivers/temperature/simulator/prototype.ttl#Mercury270"));
+  private final Map<String, Device> devicesMap = Collections.synchronizedMap(new HashMap<>());
+  private final ExecutorService executorService = Executors.newSingleThreadExecutor();
   private volatile DeviceDriverManager manager;
   private Configuration commonConfiguration;
   private CoapClient client;
-  private final Map<String, Device> devicesMap = Collections.synchronizedMap(new HashMap<>());
   private List<CoapObserveRelation> relations = new ArrayList<>();
 
   public void start() {
-    logger.debug("{} is starting!", DRIVER_NAME);
-    manager.registerDriver(info);
-    logger.debug("Try to get descrioption of devices");
-    client.setURI(commonConfiguration.getAsString(Keys.COAP_ENDPOINT));
-    //client.setTimeout(0);
-    List<String> buildings = new ArrayList<>();
-    Set<WebLink> discover = client.discover();
-    for (WebLink link : discover) {
-      if (link.getURI().matches("/\\d+" + Keys.SIMULATOR_DESCRIPTION_POSTFIX)) {
-        buildings.add(link.getURI().substring(1, link.getURI().lastIndexOf('/')));
-      }
+    try {
+      logger.debug("{} is starting!", DRIVER_NAME);
+      manager.registerDriver(info);
+
+      executorService.submit(mainThread());
+
+      logger.info("{} started!", DRIVER_NAME);
+    } catch (Throwable ex) {
+      logger.error(ex.getMessage(), ex);
     }
-
-    int count = 0;
-    for (String building : buildings) {
-      try {
-        client.setURI(commonConfiguration.getAsString(Keys.COAP_ENDPOINT) + "/" + building
-            + Keys.SIMULATOR_DESCRIPTION_POSTFIX);
-        String desq = client.get().getResponseText();
-        if (desq == null) {
-          logger.error("Can't get description! Exiting...");
-          stop();
-          return;
-        }
-        List<TemperatureDevice> devices = DriverUtils.getDevices(new JSONObject(desq));
-        for (Device dev : devices) {
-          registerDevice(dev);
-        }
-        logger.debug("{} devices are registered for building {}!", devices.size(), building);
-        count += devices.size();
-
-      } catch (JSONException ex) {
-        logger.error("Bad response format! Can't read description! Exception message is {}", ex.getMessage());
-        return;
-      }
-    }
-    logger.debug("Yeah. {} devices are registered", count);
-
-    logger.debug("Subscribe for new observations");
-    for (String building : buildings) {
-      client.setURI(commonConfiguration.getAsString(Keys.COAP_ENDPOINT) + "/" + building
-          + Keys.SIMULATOR_OBSERVATION_POSTFIX);
-      relations.add(client.observe(new CoapHandler() {
-        @Override
-        public void onLoad(CoapResponse response) {
-          try {
-
-            //List<TemperatureObservation> obs = DriverUtils.getObservations(new JSONArray(response.getResponseText()));
-            //for (TemperatureObservation o : obs) {
-            //publishNewObservation(o);
-            //}
-            DriverUtils.getAndPublishObservations(new JSONArray(response.getResponseText()), manager, devicesMap);
-          } catch (JSONException ex) {
-            logger.error("Bad response format! Can't read observations! Exception message is {}", ex.getMessage());
-          }
-        }
-
-        @Override
-        public void onError() {
-          logger.error("Something went wrong! Can't get observation");
-        }
-      }));
-    }
-
-    logger.info("{} started!", DRIVER_NAME);
   }
 
   public void stop() {
     logger.debug("{} is stopping!", DRIVER_NAME);
     logger.debug("Try to shutdown CoapClient");
-    for (CoapObserveRelation relation : relations) {
-      relation.reactiveCancel();
+    try {
+      for (CoapObserveRelation relation : relations) {
+        relation.reactiveCancel();
+      }
+      //client.shutdown();
+      devicesMap.clear();
+      executorService.shutdown();
+      try {
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+      } finally {
+        executorService.shutdownNow();
+      }
+    } catch (Throwable e) {
+      e.printStackTrace();
     }
-    //client.shutdown();
-    devicesMap.clear();
     logger.info("{} stopped!", DRIVER_NAME);
+  }
+
+  private Runnable mainThread() {
+    return () -> {
+      try {
+        logger.debug("Try to get descrioption of devices");
+        client.setURI(commonConfiguration.getAsString(Keys.COAP_ENDPOINT));
+        //client.setTimeout(0);
+        List<String> buildings = new ArrayList<>();
+        Set<WebLink> discover = client.discover();
+        for (WebLink link : discover) {
+          if (link.getURI().matches("/\\d+" + Keys.SIMULATOR_DESCRIPTION_POSTFIX)) {
+            buildings.add(link.getURI().substring(1, link.getURI().lastIndexOf('/')));
+          }
+        }
+
+        logger.debug("Going to talk to {} buildings", buildings.size());
+
+        int count = 0;
+        for (String building : buildings) {
+          try {
+            client.setURI(commonConfiguration.getAsString(Keys.COAP_ENDPOINT) + "/" + building
+                + Keys.SIMULATOR_DESCRIPTION_POSTFIX);
+            CoapResponse response = client.get();
+            if (response == null) {
+              logger.error("Response is null!!! For {} building", building);
+            } else {
+              String desq = response.getResponseText();
+              if (desq == null) {
+                logger.error("Can't get description! Exiting...");
+                stop();
+                return;
+              }
+              List<TemperatureDevice> devices = DriverUtils.getDevices(new JSONObject(desq));
+              for (Device dev : devices) {
+                registerDevice(dev);
+              }
+              logger.debug("{} devices are registered for building {}!", devices.size(), building);
+              count += devices.size();
+            }
+          } catch (JSONException ex) {
+            logger.error("Bad response format! Can't read description! Exception message is {}", ex.getMessage());
+            return;
+          }
+        }
+        logger.debug("Yeah. {} devices are registered", count);
+
+ /* logger.debug("Subscribe for new observations");
+  for (String building : buildings) {
+    client.setURI(commonConfiguration.getAsString(Keys.COAP_ENDPOINT) + "/" + building
+        + Keys.SIMULATOR_OBSERVATION_POSTFIX);
+    relations.add(client.observe(new CoapHandler() {
+      @Override
+      public void onLoad(CoapResponse response) {
+        try {
+
+          //List<TemperatureObservation> obs = DriverUtils.getObservations(new JSONArray(response.getResponseText()));
+          //for (TemperatureObservation o : obs) {
+          //publishNewObservation(o);
+          //}
+          DriverUtils.getAndPublishObservations(new JSONArray(response.getResponseText()), manager, devicesMap);
+        } catch (JSONException ex) {
+          logger.error("Bad response format! Can't read observations! Exception message is {}", ex.getMessage());
+        }
+      }
+
+      @Override
+      public void onError() {
+        logger.error("Something went wrong! Can't get observation");
+      }
+    }));
+  }*/
+      } catch (Throwable e) {
+        logger.error(e.getMessage(), e);
+      }
+    };
   }
 
   public void registerDevice(Device device) {
